c5cec9684a079f56b80167a8fab6ab5d213a3c40
mrmuhdamir <whiteforestcat96@gmail.com>
2023-04-16 08:27:53 +0800

    protect routers

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   const mode = useSelector((state) => state.mode);
   // useSelector calls the entire state in store in an object meaning state is the object
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token)); // if token exists, value will be non-zero and Boolean(non-zero) is true
 
   return (
     <div className="app">
@@ -22,8 +23,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />} // if not logged in, will be redirecetd to homepage
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
