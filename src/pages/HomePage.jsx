65d798c2fb9400dd0566ed5b1715d041dcae8d65
mrmuhdamir <whiteforestcat96@gmail.com>
2023-04-16 10:54:17 +0800

    import widget to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "./Navbar";
+import { useSelector } from "react-redux";
+import UserWidgets from "./UserWidgets";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidgets userId={_id} picturePath={picturePath} />
+          {/* _id and picturePath here are taken from store */}
+        </Box>
+      </Box>
     </Box>
   );
 };
